e8332a01a48365017cc38d19bb24649ebc3e6bbf
Hirotaka Kawata <kawata.hirotaka@linecorp.com>
2019-08-24 13:49:51 +0900

    initial answer

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,19 +4,30 @@
 // submit to:
 // https://forms.gle/3pTnurkXmpvdr6kp6
 fun answer1(list1: ListNode, list2: ListNode): ListNode {
-    return ListNode(0)
+    val node = ListNode(list1.value + list2.value)
+    list1.next?.also { l1 -> list2.next?.also { l2 -> node.next = answer1(l1, l2) } }
+    return node
 }
 
 fun answer2(y: Int, b: Int, r: Int): Int {
-    return 0
+    val a = minOf(y + 2, b + 1, r)
+    return a * 3 - 3
 }
 
 fun answer3(inputs: Int): Long {
-    return 0
+    return LongRange(0, inputs.toLong()).filterNot { it.and(1) == 1L }.reduce { a, b -> a + b }
 }
 
+@UseExperimental(ExperimentalUnsignedTypes::class)
 fun answer4(input: Sequence<Int>): Int {
-    return 0
+    val a = ULongArray(100_000_000 / 64)
+    input.forEach {
+        a[it / 64] = a[it / 64].or((1UL).shl(it.rem(64)))
+    }
+
+    return a.sumBy {
+        java.lang.Long.bitCount(it.toLong()).toUInt()
+    }.toInt()
 }
 
 // ---- submit above ----
